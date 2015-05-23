35dbc73a5c5feced5ab81cb326edeebfb9a81c42
Igor Lückel <igor.lueckel@googlemail.com>
2015-05-23 18:35:10 +0200

    Added Spring Security

---
+++ src/main/java/de/igorlueckel/vending/server/MvcConfig.java
@@ -0,0 +1,19 @@
+package de.igorlueckel.vending.server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Igor on 23.05.2015.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/logout").setViewName("logout");
+    }
+
+}
